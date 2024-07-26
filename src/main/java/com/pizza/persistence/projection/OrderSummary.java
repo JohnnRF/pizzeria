5a82c0d2e9e9faf7720ab50d6211603a83cfc65b
johnnramirez <johnnramirezf99@gmail.com>
2024-07-25 22:51:20 -0500

    first commit

---
+++ src/main/java/com/pizza/persistence/projection/OrderSummary.java
@@ -0,0 +1,14 @@
+package com.pizza.persistence.projection;
+
+import java.time.LocalDateTime;
+
+public interface OrderSummary {
+
+    Integer getIdOrder();
+    String getCustomerName();
+    LocalDateTime getOrderDate();
+    Double getOrderTotal();
+    String getPizzaNames();
+
+
+}
---
+++ src/main/java/com/pizza/persistence/repository/PizzaRepository.java
@@ -0,0 +1,36 @@
+package com.pizza.persistence.repository;
+
+import com.pizza.persistence.entity.PizzaEntity;
+import com.pizza.service.dto.UpdatePizzaPriceDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+
+    Optional<PizzaEntity> findFirstByAvailableTrueAndNameIgnoreCase(String name);
+
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
+
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
+
+    List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double precio);
+
+    int countByVeganTrue();
+
+    // update con sql nativo
+    // es importante el Modifying en los métodos update, insert y delete
+    @Query(value =
+            "UPDATE pizza "+
+            "SET price = :#{#newPizzaPrice.newPrice} "+
+            "WHERE id_pizza = :#{#newPizzaPrice.pizzaId}", nativeQuery = true)
+    @Modifying
+    void updatePrice(@Param("newPizzaPrice") UpdatePizzaPriceDto newPizzaPrice);
+
+
+}
